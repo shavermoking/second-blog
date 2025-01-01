f57e268083cfac6a7048d4e64494185a9b005084
Никита Чуркин <shavermoking@gmail.com>
2025-01-02 02:18:44 +0300

    add delete category logic

---
+++ app/Http/Controllers/Admin/Category/DeleteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Category;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\RedirectResponse;
+
+class DeleteController extends Controller
+{
+    public function __invoke(Category $category): RedirectResponse
+    {
+
+        $category->delete();
+
+        return redirect()->route('admin.category.index');
+    }
+}
