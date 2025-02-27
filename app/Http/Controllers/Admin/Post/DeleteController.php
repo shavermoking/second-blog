391b062113c42a562df876a9fd07942bc252aa4a
Никита Чуркин <shavermoking@gmail.com>
2025-02-27 18:06:58 +0300

    added Services

--- app/Http/Controllers/Admin/Post/DeleteController.php
+++ app/Http/Controllers/Admin/Post/DeleteController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\RedirectResponse;
 
-class DeleteController extends Controller
+class DeleteController extends BaseController
 {
     public function __invoke(Post $post): RedirectResponse
     {
